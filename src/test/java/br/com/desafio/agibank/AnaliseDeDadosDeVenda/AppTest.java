906d9d2f671a7084c451fec738c8749a204b1eec
Marcelo <marcelo.home19@gmail.com>
2020-11-10 12:47:26 -0300

    🎉 Inicio do projeto

---
+++ src/test/java/br/com/desafio/agibank/AnaliseDeDadosDeVenda/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.desafio.agibank.AnaliseDeDadosDeVenda;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
