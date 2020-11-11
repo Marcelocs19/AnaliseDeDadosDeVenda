99ea0faf5724913c06d770a139f86b3ae740794f
Marcelo <marcelo.home19@gmail.com>
2020-11-11 09:38:56 -0300

    ♻️ Melhorado tratamento de erros

--- src/main/java/br/com/desafio/agibank/validacao/Validacao.java
+++ src/main/java/br/com/desafio/agibank/validacao/Validacao.java
@@ -2,64 +2,81 @@ package br.com.desafio.agibank.validacao;
 
 import br.com.desafio.agibank.excecao.Erros;
 import br.com.desafio.agibank.excecao.Excecao;
+import br.com.desafio.agibank.modelos.Cliente;
+import br.com.desafio.agibank.modelos.Item;
+import br.com.desafio.agibank.modelos.Venda;
+import br.com.desafio.agibank.modelos.Vendedor;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class Validacao {
 
-	public static void validaCamposVendedor(String[] split) {
-		if (split.length < 4 || split.length > 4) {
+	public static void validaVendedor(String[] split) {
+		if (split.length != 4) {
 			throw new Excecao(Erros.MSG_ARQUIVO_VENDEDOR.getDescricao());
-		}
-		if (split[1].isBlank()) {
+		}		
+	}
+	
+	public static void validaCliente(String[] split) {
+		if (split.length != 4) {
+			throw new Excecao(Erros.MSG_ARQUIVO_CLIENTE.getDescricao());
+		}	
+	}
+	
+	public static void validaVenda(String[] split) {
+		if (split.length != 4) {
+			throw new Excecao(Erros.MSG_ARQUIVO_VENDA.getDescricao());
+		}	
+	}
+	
+	public static void validaItem(String[] split) {
+		if (split.length != 3) {
+			throw new Excecao(Erros.MSG_ARQUIVO_ITEM.getDescricao());
+		}	
+	}
+	
+	
+	public static void validaCamposVendedor(Vendedor vendedor) {
+		if (vendedor.getCpf().isBlank()) {
 			throw new Excecao(Erros.MSG_ERRO_CPF.getDescricao());
 		}
-		if (split[2].isBlank()) {
+		if (vendedor.getNome().isBlank()) {
 			throw new Excecao(Erros.MSG_ERRO_NOME.getDescricao());
 		}
-		if (split[3].isBlank()) {
+		if (vendedor.getSalario() == null) {
 			throw new Excecao(Erros.MSG_ERRO_SALARIO.getDescricao());
 		}
 	}
 
-	public static void validaCamposCliente(String[] split) {
-		if (split.length < 4 || split.length > 4) {
-			throw new Excecao(Erros.MSG_ARQUIVO_CLIENTE.getDescricao());
-		}
-		if (split[1].isBlank()) {
+	public static void validaCamposCliente(Cliente cliente) {
+		if (cliente.getCnpj().isBlank()) {
 			throw new Excecao(Erros.MSG_ERRO_CNPJ.getDescricao());
 		}
-		if (split[2].isBlank()) {
+		if (cliente.getNome().isBlank()) {
 			throw new Excecao(Erros.MSG_ERRO_NOME.getDescricao());
 		}
-		if (split[3].isBlank()) {
+		if (cliente.getBusinessArea().isBlank()) {
 			throw new Excecao(Erros.MSG_ERRO_BUSINESS_AREA.getDescricao());
 		}
 	}
 
-	public static void validaCamposVenda(String[] split) {
-		if (split.length < 4 || split.length > 4) {
-			throw new Excecao(Erros.MSG_ARQUIVO_VENDA.getDescricao());
-		}
-		if (split[1].isBlank()) {
+	public static void validaCamposVenda(Venda venda) {		
+		if (venda.getId() == null) {
 			throw new Excecao(Erros.MSG_ERRO_ID_VENDA.getDescricao());
 		}
-		if (split[3].isBlank()) {
+		if (venda.getNome().isBlank()) {
 			throw new Excecao(Erros.MSG_ERRO_NOME.getDescricao());
 		}
 	}
 
-	public static void validaCamposItens(String[] split) {
-		if (split.length < 3 || split.length > 3) {
-			throw new Excecao(Erros.MSG_ARQUIVO_ITEM.getDescricao());
-		}
-		if (split[0].isBlank()) {
+	public static void validaCamposItens(Item item) {
+		if (item.getId() == null) {
 			throw new Excecao(Erros.MSG_ERRO_ID_ITEM.getDescricao());
 		}
-		if (split[1].isBlank()) {
+		if (item.getQuantidade() == null) {
 			throw new Excecao(Erros.MSG_ERRO_QUANTIDADE.getDescricao());
 		}
-		if (split[2].isBlank()) {
+		if (item.getPreco() == null) {
 			throw new Excecao(Erros.MSG_ERRO_PRECO.getDescricao());
 		}
 	}
