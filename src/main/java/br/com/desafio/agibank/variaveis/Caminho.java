ce5125e7f2e49d39fd2ecdc3b2e032f72831d461
Marcelo Campos Silva <campos_marcelo@terceiros.sicredi.com.br>
2022-05-20 16:53:49 -0300

    �Ajustada criação de pastas e txt

--- src/main/java/br/com/desafio/agibank/variaveis/Caminho.java
+++ src/main/java/br/com/desafio/agibank/variaveis/Caminho.java
@@ -3,7 +3,7 @@ package br.com.desafio.agibank.variaveis;
 public enum Caminho {
 
     CAMINHO_ENTRADA("C:\\Users\\m_cam\\OneDrive\\Área de Trabalho\\Nova pasta\\data\\in\\"),
-    CAMINHO_SAIDA("D:\\data\\out\\");
+    CAMINHO_SAIDA("C:\\Users\\m_cam\\OneDrive\\Área de Trabalho\\Nova pasta\\data\\out\\");
 
     private String descricao;
 
